e8b5bb581e3ea97d9fb990e19b4be3c5605682ca
Lars Ekman <lars.g.ekman@est.tech>
2018-10-03 16:06:23 +0200

    Added the "images" ovl

---
+++ ovl/images/README.md
@@ -0,0 +1,133 @@
+Xcluster overlay - images
+=========================
+
+Handles pre-pulled images in cri-o
+
+`Cri-o` stores images with
+[containers/storage](https://github.com/containers/storage). This is
+very complicated and the lib requires "root" and uses hard-coded
+paths. The unpacking, e.g using
+[skopeo](https://github.com/containers/skopeo) is also horrible slow
+and causes the start-up to fail (overloads the `mqueue` I think) in
+xcluster.
+
+All-in-all it's a mess.
+
+What we want to do is to create an overlay with the
+`containers-storage` structure containing all images from
+start. Unfortunately we must do this is one file, it can not be done
+from the overlay's containing the images, and it requires "sudo".
+
+
+Usage
+-----
+
+```
+# ("images" alias defined in $h/Envsettings)
+images make coredns docker.io/nordixorg/mconnect:0.2
+xc mkcdrom [overlays...] images
+```
+
+You will be prompted for `password` for `sudo skopeo`.
+
+
+Build images
+------------
+
+There are
+[many-ways](]https://www.projectatomic.io/blog/2018/03/the-many-ways-to-build-oci-images/)
+to build OCI images. Neither is good.
+
+Uses the (configurable) root directory `/var/lib/containers/storage`.
+
+
+### Pre-pull
+
+To pre-pull images, which is necessary in `xcluster` is very hard. The
+images must be visible on;
+
+```
+> crictl --runtime-endpoint=unix:///var/run/crio/crio.sock images
+IMAGE                          TAG                 IMAGE ID            SIZE
+docker.io/nordixorg/mconnect   0.2                 d52a1329f66d7       1.89MB
+example.com/coredns            0.1                 9914622955cf0       38.7MB
+k8s.gcr.io/pause               3.1                 da86e6ba6ca19       746kB
+```
+
+The current approach is;
+
+* Use the old ACI manifest but import the images to `docker` locally.
+
+* Use `skopeo` (with sudo) to create a tar-file with the images in
+  containers-storage format.
+
+* Use the tar-file as an `xcluster` overlay.
+
+An advantage is that the `xcluster` image starts up with the images loaded
+nad ready. With `rkt` the images were installed on each node on
+startup.
+
+
+Tools
+-----
+
+The tools and `cri-o` itself uses the libraries in
+[containers/storage](https://github.com/containers/storage) to handle
+images. The library (and hence all tools) uses config in
+`/etc/containers/storage.conf` (hard-coded!). The lib also reqires
+"root".
+
+You should install `storage.conf` and `policy.json` from this overlay.
+
+### skopeo
+
+Skopeo is used by `xcluster` so it must be installed. This other tools are
+optional.
+
+Skopeo is hard to build locally but fortunately the Ubuntu package
+works fine;
+
+```
+sudo apt-add-repository ppa:projectatomic/ppa
+sudo apt-get update
+sudo apt-get install skopeo
+mkdir /tmp/x
+skopeo --insecure-policy copy docker://k8s.gcr.io/pause:3.1 dir:/tmp/x
+skopeo --insecure-policy copy docker://k8s.gcr.io/pause:3.1 oci:/tmp/x:k8s.gcr.io/pause:3.1
+```
+
+### containers-storage
+
+There is a `containers-storage` utility bundled with the lib.  Build
+with;
+
+```
+sudo apt install libdevmapper-dev -y
+cd $GOPATH/src/github.com/containers/storage
+go install ./cmd/...
+strip $GOPATH/bin/containers-storage
+```
+
+### image-tools
+
+```
+go get github.com/opencontainers/image-tools
+cd go/src/github.com/opencontainers/image-tools
+go install ./cmd/...
+strip $GOPATH/bin/oci-image-tool
+```
+
+
+### Test commands
+
+```
+skopeo --debug inspect docker://docker.io/fedora
+kubectl run hello-world --replicas=2 --labels="run=load-balancer-example" --image=gcr.io/google-samples/node-hello:1.0  --port=8080
+skopeo --debug inspect docker://gcr.io/google-samples/node-hello:1.0
+skopeo --debug inspect docker://k8s.gcr.io/pause
+skopeo --debug copy docker://gcr.io/google-samples/node-hello:1.0 \
+  containers-storage:gcr.io/google-samples/node-hello:1.0
+skopeo --debug copy docker://docker.io/library/alpine:3.8 \
+  containers-storage:docker.io/library/alpine:3.8
+kubectl create -f https://k8s.io/examples/pods/simple-pod.yaml
+```
---
+++ ovl/images/images.sh
@@ -0,0 +1,181 @@
+#! /bin/sh
+##
+## images.sh --
+##
+##   Handles OCI images in "xcluster".
+##
+## Commands;
+##
+
+prg=$(basename $0)
+dir=$(dirname $0); dir=$(readlink -f $dir)
+me=$dir/$prg
+tmp=/tmp/${prg}_$$
+
+pause="k8s.gcr.io/pause:3.1"
+
+die() {
+    echo "ERROR: $*" >&2
+    rm -rf $tmp
+    exit 1
+}
+help() {
+    grep '^##' $0 | cut -c3-
+    rm -rf $tmp
+    exit 0
+}
+test -n "$1" || help
+echo "$1" | grep -qi "^help\|-h" && help
+
+log() {
+	echo "$*" >&2
+}
+dbg() {
+	test -n "$__verbose" && echo "$prg: $*" >&2
+}
+
+cmd_env() {
+	eval $($XCLUSTER env)
+}
+
+##   docker_flush
+##     Remove ALL containers and images form docker.
+##
+cmd_docker_flush() {
+	docker rm -f $(docker ps -a -q)
+	docker rmi -f $(docker images -q)
+}
+
+cmd_in_docker() {
+	test -n "$1" || return 1
+	echo "$1" | grep -q : || die "Image must have a version"
+	skopeo inspect docker-daemon:$1 > /dev/null 2>&1
+}
+
+#   get_manifest <rootfs>
+#
+cmd_get_manifest() {
+	test -n "$1" || die "No rootfs"
+	if test -r "$1/manifest.json"; then
+		echo "$1/manifest.json"
+		return 0
+	fi
+	# Fall-back;
+	local d=$(dirname $1)
+	find $d -mindepth 1 -maxdepth 1 -name '*.json'
+}
+
+#   get_rootfs <ovl>
+#   get_rootfs <ovl/subdir>
+#
+cmd_get_rootfs() {
+	test -n "$XCLUSTER" || die 'Not set [$XCLUSTER]'
+	test -x "$XCLUSTER" || die "Not executable [$XCLUSTER]"
+	test -n "$1" || die "No ovl"
+	local d ovl rootfs=image
+	if echo "$1" | grep -q / ; then
+		ovl=$(echo $1 | cut -d/ -f1)
+		rootfs=$(echo $1 | cut -d/ -f2)
+	else
+		ovl=$1
+	fi
+	d=$($XCLUSTER ovld $ovl) || die "Can't find ovl [$ovl]"
+	test -d $d/$rootfs || die "Not a directory [$d/$rootfs]"
+	echo $d/$rootfs
+}
+
+##   mkimage --manifest=manifest <dir>
+##     Create an image in local docker from the <dir>. An executable
+##     "tar" script must exist in the directory.
+##
+cmd_mkimage() {
+	test -n "$1" || die "No rootfs"
+	test -d "$1" || die "Not a directory [$1]"
+	test -x "$1/tar" || die "Not executable [$1/tar]"
+	test -n "$__manifest" || die 'Manifest missing'
+	which jq > /dev/null || die "Not executable [jq]"
+
+	local n v c tar
+	tar="$(readlink -f $1)/tar"
+	v=$(jq -r '.labels[] | select(.name=="version") | .value' $__manifest)
+	n=$(jq -r .name $__manifest)
+	c=$(jq -rc '.app.exec' $__manifest)
+
+	if $(cmd_in_docker "$n:$v"); then
+		log "Cached [$n:$v]"
+	else
+		$tar - | docker import -c "CMD $c" - $n:$v > /dev/null
+	fi
+	echo "$n:$v"
+}
+
+#   mktar --user=user [--tar=-] <images...>
+#     Create a tar file with a container-storage structure.
+#
+cmd_mktar() {
+	test -n "$__tar" || __tar=-
+	test -n "$__user" || die "No --user"
+	test $(id -u) -eq 0 || die 'Must run as "root"'
+	rm -rf /tmp/var > /dev/null 2>&1
+	local f n
+	for f in $@; do
+		log "Adding [$f]"
+        skopeo --insecure-policy copy docker-daemon:$f \
+            containers-storage:$f > /dev/null 2>&1 || die "skopeo failed [$f]"
+	done
+
+	tar -C /tmp -cf $__tar var
+	test "$__tar" != "-" && chown $(id -u $__user):$(id -g $__user) $__tar
+}
+
+##   make [--tar=$XCLUSTER_TMP/images.tar <items...>
+##     Create a tar file with the container-storage structure for the
+##     passed image items.
+##
+cmd_make() {
+	cmd_env
+	test -n "$__tar" || __tar=$XCLUSTER_TMP/images.tar
+	local n rootfs img=''
+	mkdir -p $tmp
+	for n in $pause $@; do
+		if echo "$n" | grep -q : ; then
+			cmd_in_docker "$n" || docker pull "$n" || die "Pull failed [$n]"
+			img="$img $n"
+		else
+			rootfs=$(cmd_get_rootfs $n) || return 1
+			manifest=$(cmd_get_manifest $rootfs)
+			test -n "$manifest" || die "Can't find manifest [$n]"
+			img="$img $($me mkimage --manifest=$manifest $rootfs)" || return 1
+		fi
+	done
+	sudo $me mktar --user=$USER --tar=$__tar $img
+	rm -rf $tmp
+	return 0
+}
+
+
+# Get the command
+cmd=$1
+shift
+grep -q "^cmd_$cmd()" $0 $hook || die "Invalid command [$cmd]"
+
+while echo "$1" | grep -q '^--'; do
+    if echo $1 | grep -q =; then
+	o=$(echo "$1" | cut -d= -f1 | sed -e 's,-,_,g')
+	v=$(echo "$1" | cut -d= -f2-)
+	eval "$o=\"$v\""
+    else
+	o=$(echo "$1" | sed -e 's,-,_,g')
+	eval "$o=yes"
+    fi
+    shift
+done
+unset o v
+long_opts=`set | grep '^__' | cut -d= -f1`
+
+# Execute command
+trap "die Interrupted" INT TERM
+cmd_$cmd "$@"
+status=$?
+rm -rf $tmp
+exit $status
