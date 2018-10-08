095c1b17a7f9fa24625b4ad5580d4f1b47a0c651
Lars Ekman <lars.g.ekman@est.tech>
2018-10-08 13:21:37 +0200

    Avoid ipv6 for user-space net if not supported

--- config/net-setup-userspace.sh
+++ config/net-setup-userspace.sh
@@ -12,6 +12,18 @@ net_user() {
 	echo "-net user,vlan=$net,net=192.168.0.$nodeid/24,host=192.168.0.250,ipv6-net=2000::/64,ipv6-host=2000::250,hostfwd=tcp:127.0.0.1:$pt-192.168.0.$nodeid:23,hostfwd=tcp:127.0.0.1:$ps-192.168.0.$nodeid:22"
 }
 
+net_user_old_qemu() {
+	local nodeid=$1
+	local net=$2
+	local b0=$(printf '%x' $nodeid)
+	test -n "$XCLUSTER_TELNET_BASE" || XCLUSTER_TELNET_BASE=12000
+	local pt=$((XCLUSTER_TELNET_BASE+nodeid))
+	test -n "$XCLUSTER_SSH_BASE" || XCLUSTER_SSH_BASE=12100
+	local ps=$((XCLUSTER_SSH_BASE+nodeid))
+	echo "-net nic,vlan=$net,macaddr=0:0:0:1:$net:$b0,model=virtio"
+	echo "-net user,vlan=$net,net=192.168.0.$nodeid/24,host=192.168.0.250,hostfwd=tcp:127.0.0.1:$pt-192.168.0.$nodeid:23,hostfwd=tcp:127.0.0.1:$ps-192.168.0.$nodeid:22"
+}
+
 net_uml() {
 	local nodeid=$1
 	local net=$2
@@ -23,7 +35,11 @@ net_uml() {
 }
 
 if test "$2" -eq 0; then
-	net_user $1 $2
+	if kvm -version grep -qE 'QEMU emulator version 2\.[1-5]\.'; then
+		net_user_old_qemu $1 $2
+	else
+		net_user $1 $2
+	fi
 else
 	net_uml $1 $2
 fi
