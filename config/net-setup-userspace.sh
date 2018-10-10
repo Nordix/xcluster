00dafe26ce1a68de87cd56f302b02ebec1dbbd9a
Lars Ekman <lars.g.ekman@est.tech>
2018-10-10 16:33:16 +0200

    Fixed k8s api port-forwaring for old qemu
    Without this kubectl outside the cluster does not work
    on Ubuntu 16.04

--- config/net-setup-userspace.sh
+++ config/net-setup-userspace.sh
@@ -33,7 +33,7 @@ net_user_old_qemu() {
 		k8s=",hostfwd=tcp:127.0.0.1:$pk-192.168.0.1:8080"
 	fi
 	echo "-net nic,vlan=$net,macaddr=0:0:0:1:$net:$b0,model=virtio"
-	echo "-net user,vlan=$net,net=192.168.0.$nodeid/24,host=192.168.0.250,hostfwd=tcp:127.0.0.1:$pt-192.168.0.$nodeid:23,hostfwd=tcp:127.0.0.1:$ps-192.168.0.$nodeid:22"
+	echo "-net user,vlan=$net,net=192.168.0.$nodeid/24,host=192.168.0.250,hostfwd=tcp:127.0.0.1:$pt-192.168.0.$nodeid:23,hostfwd=tcp:127.0.0.1:$ps-192.168.0.$nodeid:22$k8s"
 }
 
 net_uml() {
