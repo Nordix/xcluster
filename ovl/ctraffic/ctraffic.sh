802b41f509a8102664152bd9e3b5c8163be7c232
Lars Ekman <lars.g.ekman@est.tech>
2020-01-29 16:21:38 +0100

    ovl/ctraffic; Updated for ctraffic v1.0.0 and added tests

---
+++ ovl/ctraffic/ctraffic.sh
@@ -0,0 +1,166 @@
+#! /bin/sh
+##
+## ctraffic.sh --
+##
+##   Help script for the xcluster ovl/ctraffic.
+##
+## Commands;
+##
+
+prg=$(basename $0)
+dir=$(dirname $0); dir=$(readlink -f $dir)
+me=$dir/$prg
+tmp=/tmp/${prg}_$$
+
+die() {
+    echo "ERROR: $*" >&2
+    rm -rf $tmp
+    exit 1
+}
+help() {
+    grep '^##' $0 | cut -c3-
+    rm -rf $tmp
+    exit 0
+}
+test -n "$1" || help
+echo "$1" | grep -qi "^help\|-h" && help
+
+log() {
+	echo "$prg: $*" >&2
+}
+dbg() {
+	test -n "$__verbose" && echo "$prg: $*" >&2
+}
+
+##  env
+##    Print environment.
+##
+cmd_env() {
+
+	test -n "$__tag" || __tag="registry.nordix.org/cloud-native/ctraffic:latest"
+
+	if test "$cmd" = "env"; then
+		set | grep -E '^(__.*)='
+		retrun 0
+	fi
+
+	test -n "$__cluster_domain" || __cluster_domain=xcluster
+	test -n "$XCLUSTER" || die 'Not set [$XCLUSTER]'
+	test -x "$XCLUSTER" || die "Not executable [$XCLUSTER]"
+	eval $($XCLUSTER env)
+}
+
+##   test --list
+##   test [--xterm] [test...] > logfile
+##     Exec tests
+##
+cmd_test() {
+	if test "$__list" = "yes"; then
+        grep '^test_' $me | cut -d'(' -f1 | sed -e 's,test_,,'
+        return 0
+    fi
+
+	cmd_env
+    start=starts
+    test "$__xterm" = "yes" && start=start
+    rm -f $XCLUSTER_TMP/cdrom.iso
+
+    if test -n "$1"; then
+        for t in $@; do
+            test_$t
+        done
+    else
+        for t in basic; do
+            test_$t
+        done
+    fi      
+
+    now=$(date +%s)
+    tlog "Xcluster test ended. Total time $((now-begin)) sec"
+
+}
+
+test_start() {
+	test -n "$__mode" || __mode=dual-stack
+	xcluster_prep $__mode
+	xcluster_start ctraffic
+
+	otc 1 check_namespaces
+	otc 1 check_nodes
+	otc 1 check_metric_server
+}
+
+
+test_basic() {
+	tlog "=== ctraffic: Basic test"
+	test "$__no_start" != "yes" && test_start
+
+	otc 1 start_ctraffic
+	otc 2 internal_traffic_ipv4
+	otc 2 internal_traffic_ipv6
+
+	otc 1 assign_lb_ip
+	otc 201 routes
+	otc 201 external_traffic_ipv4
+	otc 201 external_traffic_ipv6	
+
+	otc 201 lossy_traffic_ipv4
+
+	xcluster_stop
+}
+
+
+cmd_otc() {
+	test -n "$__vm" || __vm=2
+	otc $__vm $@
+}
+
+##   mkimage [--tag=registry.nordix.org/cloud-native/ctraffic:latest]
+##     Create the docker image and upload it to the local registry.
+##
+cmd_mkimage() {
+	cmd_env
+	local imagesd=$($XCLUSTER ovld images)
+	$imagesd/images.sh mkimage --force --upload --strip-host --tag=$__tag $dir/image
+}
+
+##   go_build
+##     Build local go program. Output to ./image/default/bin
+##
+cmd_go_build() {
+	mkdir -p $dir/image/default/bin
+	cd $dir/go
+	GO111MODULE=on CGO_ENABLED=0 GOOS=linux go build \
+		-ldflags "-extldflags '-static' -X main.version=$(date +%F:%T)" \
+		-o ../image/default/bin/list-nodes ./cmd/list-nodes/main.go
+	strip ../image/default/bin/list-nodes
+}
+
+. $($XCLUSTER ovld test)/default/usr/lib/xctest
+indent=''
+
+# Get the command
+cmd=$1
+shift
+grep -q "^cmd_$cmd()" $0 $hook || die "Invalid command [$cmd]"
+
+while echo "$1" | grep -q '^--'; do
+    if echo $1 | grep -q =; then
+	o=$(echo "$1" | cut -d= -f1 | sed -e 's,-,_,g')
+	v=$(echo "$1" | cut -d= -f2-)
+	eval "$o=\"$v\""
+    else
+	o=$(echo "$1" | sed -e 's,-,_,g')
+	eval "$o=yes"
+    fi
+    shift
+done
+unset o v
+long_opts=`set | grep '^__' | cut -d= -f1`
+
+# Execute command
+trap "die Interrupted" INT TERM
+cmd_$cmd "$@"
+status=$?
+rm -rf $tmp
+exit $status
