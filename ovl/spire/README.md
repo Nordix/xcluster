0239d3557edd29e9eb9dbb0b5b4f160f0eb05c47
Lars Ekman <lars.g.ekman@est.tech>
2021-12-15 14:08:43 +0100

    ovl/spire; added

---
+++ ovl/spire/README.md
@@ -0,0 +1,62 @@
+# Xcluster/ovl - spire
+
+[Spire](https://spiffe.io/docs/latest/spire-about/spire-concepts/) in xcluster.
+
+The new [registrar](https://github.com/spiffe/spire/tree/main/support/k8s/k8s-workload-registrar)
+configuration is used.
+
+The main intended usage is for [nsm-test](https://github.com/Nordix/nsm-test/tree/master/ovl). We attempt to be as close to the older NSM spire setup as possible
+so the "spiffeid" is;
+```
+spiffe://example.org/ns/default/sa/default
+```
+
+The "default" service account is allowed and "identity_template" is set as;
+```
+    identity_template = "ns/{{.Pod.Namespace}}/sa/{{.Pod.ServiceAccount}}"
+    #identity_template_label = "spiffe.io/spiffe-id"  (removed)
+```
+
+Then a default "spiffeid" is created with;
+```
+apiVersion: spiffeid.spiffe.io/v1beta1
+kind: SpiffeID
+metadata:
+  name: default-spiffeid
+  namespace: default
+spec:
+  parentId: "spiffe://example.org/spire/server"
+  selector:
+    namespace: default
+    serviceAccount: default
+  spiffeId: "spiffe://example.org/ns/default/sa/default"
+```
+
+## Usage
+
+Load the local registry;
+```
+for n in $(images lreg_missingimages .); do
+  images lreg_cache $n
+done
+```
+
+Manual test;
+```
+./spire.sh test start_registrar > $log
+# On a vm;
+kubectl get spiffeid -A
+kubectl get spiffeid -n spire vm-001 -o json
+kubectl exec spire-server-0 -n spire -c spire-server -- ./bin/spire-server entry show
+```
+
+### Call from another ovl
+
+Assuming the `ovl/test` is used, make sure `ovl/spire` is included and add in
+your test start;
+```
+otcprog=spire_test
+otc 1 start_spire_registrar
+unset otcprog
+```
+
---
+++ ovl/spire/spire.sh
@@ -0,0 +1,154 @@
+#! /bin/sh
+##
+## spire.sh --
+##
+##   Help script for the xcluster ovl/spire.
+##
+## Commands;
+##
+
+prg=$(basename $0)
+dir=$(dirname $0); dir=$(readlink -f $dir)
+me=$dir/$prg
+tmp=/tmp/${prg}_$$
+
+die() {
+    echo "ERROR: $*" >&2
+    rm -rf $tmp
+    exit 1
+}
+help() {
+    grep '^##' $0 | cut -c3-
+    rm -rf $tmp
+    exit 0
+}
+test -n "$1" || help
+echo "$1" | grep -qi "^help\|-h" && help
+
+log() {
+	echo "$prg: $*" >&2
+}
+dbg() {
+	test -n "$__verbose" && echo "$prg: $*" >&2
+}
+
+##  env
+##    Print environment.
+##
+cmd_env() {
+
+	test -n "$__tag" || __tag="registry.nordix.org/cloud-native/spire:latest"
+
+	if test "$cmd" = "env"; then
+		set | grep -E '^(__.*)='
+		return 0
+	fi
+
+	test -n "$xcluster_DOMAIN" || xcluster_DOMAIN=xcluster
+	test -n "$XCLUSTER" || die 'Not set [$XCLUSTER]'
+	test -x "$XCLUSTER" || die "Not executable [$XCLUSTER]"
+	eval $($XCLUSTER env)
+}
+
+##   test --list
+##   test [--xterm] [test...] > logfile
+##     Exec tests
+##
+cmd_test() {
+	if test "$__list" = "yes"; then
+        grep '^test_' $me | cut -d'(' -f1 | sed -e 's,test_,,'
+        return 0
+    fi
+
+	cmd_env
+    start=starts
+    test "$__xterm" = "yes" && start=start
+    rm -f $XCLUSTER_TMP/cdrom.iso
+
+    if test -n "$1"; then
+        for t in $@; do
+            test_$t
+        done
+    else
+		test_default
+    fi      
+
+    now=$(date +%s)
+    tlog "Xcluster test ended. Total time $((now-begin)) sec"
+
+}
+
+test_start_empty() {
+	test "$__version" || __version=0.10.0
+	export __version
+	export xcluster___version=$__version
+	test -n "$__mode" || __mode=dual-stack
+	export xcluster___mode=$__mode
+	xcluster_prep $__mode
+	xcluster_start k8s-test spire
+
+	otc 1 check_namespaces
+	otc 1 check_nodes
+	otcr vip_routes
+}
+test_start_registrar() {
+	test_start_empty
+	otc 1 start_spire_registrar
+}
+
+test_default() {
+	tlog "=== spire: Basic test"
+	test_start_registrar
+	xcluster_stop
+}
+
+##   Older ways of loading spire;
+##   generate_manifests [--version=1.0.2]
+##     Generate manifests from helm charts.
+##   kustomize
+##
+cmd_generate_manifests() {
+        local dst=$dir/default/etc/kubernetes/spire
+		test -n "$__version" || __version=1.0.2
+		test -d "$dir/helm/$__version" || die "Version not found [$__version]"
+        mkdir -p $dst
+		helm template $dir/helm/$__version --generate-name --namespace spire \
+			--create-namespace > $dst/spire.yaml 2> /dev/null || die helm
+}
+
+cmd_kustomize() {
+    local dst=$dir/default/etc/kubernetes/spire
+	local url=https://github.com/spiffe/spire-tutorials/k8s/quickstart
+    mkdir -p $dst
+	kubectl kustomize $url > $dst/spire.yaml
+	sed -i -e 's,1.0.0,1.0.2,' $dst/spire.yaml
+}
+
+. $($XCLUSTER ovld test)/default/usr/lib/xctest
+indent=''
+
+# Get the command
+cmd=$1
+shift
+grep -q "^cmd_$cmd()" $0 $hook || die "Invalid command [$cmd]"
+
+while echo "$1" | grep -q '^--'; do
+    if echo $1 | grep -q =; then
+	o=$(echo "$1" | cut -d= -f1 | sed -e 's,-,_,g')
+	v=$(echo "$1" | cut -d= -f2-)
+	eval "$o=\"$v\""
+    else
+	o=$(echo "$1" | sed -e 's,-,_,g')
+	eval "$o=yes"
+    fi
+    shift
+done
+unset o v
+long_opts=`set | grep '^__' | cut -d= -f1`
+
+# Execute command
+trap "die Interrupted" INT TERM
+cmd_$cmd "$@"
+status=$?
+rm -rf $tmp
+exit $status
