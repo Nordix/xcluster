ab659383d17f2ede286e2233f4129315699884bc
Sriram Yagnaraman <sriram.yagnaraman@est.tech>
2022-11-17 13:43:35 +0100

    ovl/qemu-sriov: use igb-device branch

--- ovl/qemu-sriov/config/net-setup-netns.sh
+++ ovl/qemu-sriov/config/net-setup-netns.sh
@@ -9,21 +9,26 @@ ip link show dev $dev > /dev/null 2>&1 || \
 tap=${dev}_t$nodeid
 b1=$n
 
-if ! ip link show dev $tap > /dev/null 2>&1; then
-	ip tuntap add $tap mode tap user $USER
-	ip link set mtu $__mtu dev $tap
-	ip link set dev $tap master $dev
-	ip link set up $tap
-fi
-
-dev=igb
+dut=igb
 
 b0=$(printf '%02x' $nodeid)
 if test $n -gt 0 ; then
-	echo "$opt -device pcie-root-port,slot=2,id=pcie_port.2"
-	echo "$opt -netdev tap,id=net$n,script=no,downscript=/tmp/rmtap,ifname=$tap"
-	echo "$opt -device $dev,bus=pcie_port.2,netdev=net$n,mac=00:00:00:01:0$b1:$b0"
+	if ! ip link show dev $tap > /dev/null 2>&1; then
+		ip tuntap add $tap mode tap user $USER multi_queue
+		ip link set mtu $__mtu dev $tap
+		ip link set dev $tap master $dev
+		ip link set up $tap
+	fi
+	echo "$opt -device pcie-root-port,slot=$n,id=pcie_port.$n"
+	echo "$opt -netdev tap,id=net$n,script=no,downscript=/tmp/rmtap,ifname=$tap,queues=2"
+	echo "$opt -device $dut,bus=pcie_port.$n,netdev=net$n,mac=00:00:00:01:0$b1:$b0"
 else
+	if ! ip link show dev $tap > /dev/null 2>&1; then
+		ip tuntap add $tap mode tap user $USER
+		ip link set mtu $__mtu dev $tap
+		ip link set dev $tap master $dev
+		ip link set up $tap
+	fi
 	echo "$opt -netdev tap,id=net$n,script=no,downscript=/tmp/rmtap,ifname=$tap"
 	echo "$opt -device virtio-net-pci,netdev=net$n,mac=00:00:00:01:0$b1:$b0"
 	#echo "$opt -device e1000,netdev=net$n,mac=00:00:00:01:0$b1:$b0"
