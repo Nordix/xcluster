49379f270ad05885a7802b90e82b54494f7e05ea
Lars Ekman <lars.g.ekman@est.tech>
2023-04-12 11:34:19 +0200

    Update cri-o version

--- xcadmin.sh
+++ xcadmin.sh
@@ -243,7 +243,7 @@ cmd_k8s_archives() {
 	echo $ARCHIVE/mconnect.xz
 	echo $ARCHIVE/xcluster-cache.tar
 	echo $ARCHIVE/assign-lb-ip.xz
-	echo $ARCHIVE/crio-v1.22.0.tar.gz
+	echo $ARCHIVE/cri-o.amd64.v1.25.1.tar.gz
 }
 
 cmd_k8s_workspace() {
